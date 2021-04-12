f67153d6c8765bfa8a4635007f71bf6adfa3fe2b
Ben Muschol <ben.muschol@airbnb.com>
2021-04-11 21:05:44 -0400

    wip

--- app/controllers/voter_controller.rb
+++ app/controllers/voter_controller.rb
@@ -2,7 +2,7 @@
 
 class VoterController < ApplicationController
   SKIP_WARN_THRESHOLDS = Set.new(Rails.configuration.rewards.skip_warnings)
-  SUCCESS_THRESHOLDS   = Set.new(Rails.configuration.rewards.videos.keys)
+  SUCCESS_THRESHOLDS   = Set.new([])
 
   before_action :authorize_and_set_contact, only: [:show, :update]
   before_action :migrate_voters_seen, only: [:next, :show]
